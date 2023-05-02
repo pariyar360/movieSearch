795829c53d4f883c69dcca225952e74a7a0982ff
Prakash <pariyar360@gmail.com>
2023-05-02 19:25:21 +1000

    linked the pages and minor adjustments to results display

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const moviesEl = document.querySelector(".movies")
+const hidden = document.querySelector(".hidden")
 const movieListEl = document.querySelector(".movies__list")
 
 async function querySubmit() {
     let query = document.getElementById("query").value;
-    console.log(query)
     renderMovies(query)
+    hidden.classList.remove("hidden")
 }
 
 async function renderMovies(query) {
     const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e792d444&s=${query}`)
     const queryData = await movies.json()
     const moviesData = queryData["Search"]
-    movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie))
+    movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("")
 }
 
 
