62d9089117b57fc9bc7f089d044f1fd31d4f5f23
Prakash <pariyar360@gmail.com>
2023-05-03 10:59:39 +1000

    minor changes including logo error and the search query not deleting when reloading fixed

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const hidden = document.querySelector(".hidden")
 const movieListEl = document.querySelector(".movies__list")
 const query = localStorage.getItem("query")
 
+if (query) {
+    renderMovies(query)
+    localStorage.removeItem("query")
+}
+
 async function querySubmit() {
     const query = document.getElementById("query").value;
     renderMovies(query)
 }
 
 async function renderMovies(query) {
-    console.log(query)
     hidden.classList.remove("hidden")
     const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e792d444&s=${query}`)
     const queryData = await movies.json()
     const moviesData = queryData["Search"]
+    if (!moviesData) {
+        movieListEl.innerHTML = `<h2>Movie not in database</h2>`
+    }
     movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("")
 }
 
@@ -23,5 +30,3 @@ function movieHTML(movie) {
                 <p class="movie__year">${movie.Year}</p>
             </div>`
 }
-
-renderMovies(query)
